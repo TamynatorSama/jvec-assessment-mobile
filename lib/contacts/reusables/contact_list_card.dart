98f22e0dc716efa51abb6015fa4d9dacef526de1
tamynatorSama <kolawoletamilore1@gmail.com>
2023-10-21 17:28:25 +0100

    fix: changed the contact listing card to fit in

--- lib/contacts/reusables/contact_list_card.dart
+++ lib/contacts/reusables/contact_list_card.dart
@@ -1,3 +1,4 @@
+import 'package:contact_app/contacts/contact_details.dart';
 import 'package:contact_app/utils/app_theme.dart';
 import 'package:flutter/material.dart';
 
@@ -9,26 +10,29 @@ class ContactListCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-      padding: const EdgeInsets.only(bottom:20.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        CircleAvatar(
-          radius: 25,
-          backgroundColor: AppTheme.btnColor,
-          child: const Icon(Icons.person,color: Colors.white,),
+    return InkWell(
+      onTap: ()=>Navigator.of(context).push(MaterialPageRoute(builder: (context)=>const ContactDetails())),
+      child: Padding(
+        padding: const EdgeInsets.only(bottom:20.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 25,
+            backgroundColor: AppTheme.btnColor,
+            child: const Icon(Icons.person,color: Colors.white,),
+          ),
+          const SizedBox(width: 20,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(label,style: AppTheme.mainTextStyle.override(color:Colors.grey),),
+              Text(value,style: AppTheme.headerTextStyle.override(fontSize: 18),)
+            ],
+          ),
+          
+        ],
         ),
-        const SizedBox(width: 20,),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(label,style: AppTheme.mainTextStyle.override(color:Colors.grey),),
-            Text(value,style: AppTheme.headerTextStyle.override(fontSize: 18),)
-          ],
-        ),
-        
-      ],
       ),
     );
   }
