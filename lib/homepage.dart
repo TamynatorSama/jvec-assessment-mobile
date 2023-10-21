24c967606fa85b2d2aa55b385b23b2e464bf18a2
tamynatorSama <kolawoletamilore1@gmail.com>
2023-10-21 17:27:16 +0100

    feat: added home page ui

---
+++ lib/homepage.dart
@@ -0,0 +1,37 @@
+import 'package:contact_app/contacts/contact_list_view.dart';
+import 'package:contact_app/utils/app_theme.dart';
+import 'package:contact_app/utils/custom_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppTheme.backgroundColor,
+      body: Column(
+        children: [
+          SvgPicture.asset('assets/images/contact.svg',),
+          // const SizedBox(height: ,),
+          Text("Welcome",style: AppTheme.headerTextStyle.override(fontSize: 32),),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Text(
+              "Experience the convenience of a unified contact management solution, where you can effortlessly create, access, and organize all your contacts in one user-friendly app, eliminating the hassle of scattered information."
+              // "Effortlessly manage your contacts in a single, user-friendly app. Say goodbye to scattered information and experience seamless organization."
+              // "Easily create, access, and organize all your contacts in one place with our user-friendly app, simplifying contact management."
+              // "Streamline your communication by effortlessly creating, accessing, and organizing all your contacts in one place with the user-friendly contact app. Say goodbye to scattered contact information and welcome the convenience of a centralized hub for seamless management."
+              ,style: AppTheme.mainTextStyle.override(color: Colors.grey),textAlign: TextAlign.center,),
+          ),
+          const Spacer(),
+          CustomButton(label: "Continue", onTap: ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=>const ContactListView()))),
+          SizedBox(
+                    height: MediaQuery.of(context).padding.top + 10,
+                  )
+        ],
+      ),
+    );
+  }
+}
