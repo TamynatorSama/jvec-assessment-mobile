14f689550feaf214906ac5b98ddfc38d75c05ab3
tamynatorSama <kolawoletamilore1@gmail.com>
2023-10-22 11:01:20 +0100

    feat: added conditional route feature for guest and signedin user

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,34 @@
+import 'package:contact_app/app_provider.dart';
+import 'package:contact_app/contacts/contact_list_view.dart';
 import 'package:contact_app/homepage.dart';
+import 'package:contact_app/model/user_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
-    statusBarColor: Colors.transparent,
-    systemNavigationBarColor: Colors.transparent
-  ));
+      statusBarColor: Colors.transparent,
+      systemNavigationBarColor: Colors.transparent));
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  await UserData.initialize();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage()
+    return ChangeNotifierProvider<AppProvider>(
+      create: (context) => AppProvider(),
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: UserData.token.isEmpty
+              ? const HomePage()
+              : const ContactListView()),
     );
   }
 }
