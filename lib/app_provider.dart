404817f515f9ef5886e518d617779c39249b0773
tamynatorSama <kolawoletamilore1@gmail.com>
2023-10-22 12:38:02 +0100

    feat: accounted for empty list state

---
+++ lib/app_provider.dart
@@ -0,0 +1,14 @@
+import 'package:contact_app/model/contact_model.dart';
+import 'package:flutter/material.dart';
+
+class AppProvider extends ChangeNotifier{
+
+  List<ContactInfo> contacts=[];
+
+  Future<void> getContacts()async{
+
+    
+  } 
+
+
+}
--- lib/contacts/contact_list_view.dart
+++ lib/contacts/contact_list_view.dart
@@ -1,10 +1,12 @@
 import 'package:azlistview/azlistview.dart';
+import 'package:contact_app/app_provider.dart';
 import 'package:contact_app/contacts/create_contact.dart';
 import 'package:contact_app/contacts/reusables/contact_list_Card.dart';
 import 'package:contact_app/model/contact_model.dart';
 import 'package:contact_app/utils/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
 
 class ContactListView extends StatelessWidget {
   const ContactListView({super.key});
@@ -17,68 +19,89 @@ class ContactListView extends StatelessWidget {
      SuspensionUtil.setShowSuspensionStatus(data);
 
 
-    return Scaffold(
-      backgroundColor: AppTheme.backgroundColor,
-      body: Padding(padding: EdgeInsets.fromLTRB(24, MediaQuery.of(context).padding.top+5, 24, MediaQuery.of(context).padding.top+10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-        Text("Contacts",style: AppTheme.headerTextStyle.override(fontSize: 32),),
-        const SizedBox(height: 20,),
-        TextFormField(
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: AppTheme.btnColor,
-            hintText: "Search...",
-            hintStyle: AppTheme.headerTextStyle.override(fontSize: 16),
-            prefixIcon: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: SvgPicture.string('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none"><path fill="grey" d="M19 11a8 8 0 1 1-16 0a8 8 0 0 1 16 0Z" opacity=".16"/><path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m21 21l-4.343-4.343m0 0A8 8 0 1 0 5.343 5.343a8 8 0 0 0 11.314 11.314Z"/></g></svg>',color: const Color.fromARGB(255, 199, 199, 199),),
-            ),
-            contentPadding: const EdgeInsets.symmetric(horizontal: 15,vertical: 10),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-              borderSide: const BorderSide(width: 1.5,color: Color.fromARGB(255, 222, 222, 222))
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-              borderSide: const BorderSide(width: 1.5,color: Color.fromARGB(255, 222, 222, 222))
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-              borderSide: const BorderSide(width: 1.5,color: Color.fromARGB(255, 222, 222, 222))
-            )
-          ),
-        style: AppTheme.headerTextStyle.override(fontSize: 16),
-        ),
-        const SizedBox(height: 40,),
-Expanded(child:
-        AzListView(
-          indexBarOptions: IndexBarOptions(
-            needRebuild: true,
-            indexHintAlignment: Alignment.centerRight,
-            selectTextStyle: AppTheme.headerTextStyle
-          ),
-          indexBarWidth: 10,
-          data: data, itemCount: data.length, itemBuilder: (context,index)=>Column(
+    return Consumer<AppProvider>(
+      builder: (context,provider,value) {
+        return Scaffold(
+          backgroundColor: AppTheme.backgroundColor,
+          body: Padding(padding: EdgeInsets.fromLTRB(24, MediaQuery.of(context).padding.top+5, 24, MediaQuery.of(context).padding.top+10),
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              if(data[index].isShowSuspension)
-                Column(
-                  children: [
-                    Text(data[index].getSuspensionTag(),style: AppTheme.headerTextStyle.override(fontSize: 16),),
-                    const SizedBox(height: 10,)
-                  ],
+            Text("Contacts",style: AppTheme.headerTextStyle.override(fontSize: 32),),
+            if(provider.contacts.isNotEmpty)
+            Column(
+              children: [
+                const SizedBox(height: 20,),
+                TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: AppTheme.btnColor,
+                hintText: "Search...",
+                hintStyle: AppTheme.headerTextStyle.override(fontSize: 16),
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: SvgPicture.string('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none"><path fill="grey" d="M19 11a8 8 0 1 1-16 0a8 8 0 0 1 16 0Z" opacity=".16"/><path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m21 21l-4.343-4.343m0 0A8 8 0 1 0 5.343 5.343a8 8 0 0 0 11.314 11.314Z"/></g></svg>',color: const Color.fromARGB(255, 199, 199, 199),),
                 ),
-              ContactListCard(label: "${data[index].firstName} ${data[index].lastName}", value: data[index].phoneNumber)
-            ],
-          )))
-      ]),),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: AppTheme.btnColor,
-        onPressed: ()=>Navigator.of(context).push(MaterialPageRoute(builder: (context)=>const CreateContact())),
-        child: const Icon(Icons.add,color: Colors.white,),
-        ),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(width: 1.5,color: Color.fromARGB(255, 222, 222, 222))
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(width: 1.5,color: Color.fromARGB(255, 222, 222, 222))
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(width: 1.5,color: Color.fromARGB(255, 222, 222, 222))
+                )
+              ),
+            style: AppTheme.headerTextStyle.override(fontSize: 16),
+            ),
+            const SizedBox(height: 40,),
+              ],
+            ),
+Expanded(child:
+            provider.contacts.isEmpty?Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              Opacity(
+                opacity: 0.5,
+                child: SvgPicture.asset('assets/images/Empty-pana.svg',fit: 
+                BoxFit.contain,height: 400,)),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal:24.0),
+                  child: Text("You haven't added any contact yet, click the + icon at the buttom to get started",style: AppTheme.headerTextStyle.override(fontSize: 15,color: Colors.grey),textAlign: TextAlign.center,),
+                )
+              ],
+            ) :AzListView(
+              indexBarOptions: IndexBarOptions(
+                needRebuild: true,
+                indexHintAlignment: Alignment.centerRight,
+                selectTextStyle: AppTheme.headerTextStyle
+              ),
+              indexBarWidth: 10,
+              data: data, itemCount: data.length, itemBuilder: (context,index)=>Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if(data[index].isShowSuspension)
+                    Column(
+                      children: [
+                        Text(data[index].getSuspensionTag(),style: AppTheme.headerTextStyle.override(fontSize: 16),),
+                        const SizedBox(height: 10,)
+                      ],
+                    ),
+                  ContactListCard(label: "${data[index].firstName} ${data[index].lastName}", value: data[index].phoneNumber)
+                ],
+              )))
+          ]),),
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: AppTheme.btnColor,
+            onPressed: ()=>Navigator.of(context).push(MaterialPageRoute(builder: (context)=>const CreateContact())),
+            child: const Icon(Icons.add,color: Colors.white,),
+            ),
+        );
+      }
     );
   }
 }
