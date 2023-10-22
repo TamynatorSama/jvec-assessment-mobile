fa352a2cafb11e140f46208b36312065acbf8797
tamynatorSama <kolawoletamilore1@gmail.com>
2023-10-22 11:52:06 +0100

    chore: created functions to show login and signup

--- lib/auth/modal_function.dart
+++ lib/auth/modal_function.dart
@@ -4,33 +4,40 @@ import 'package:contact_app/auth/login_modal.dart';
 import 'package:contact_app/auth/signup.dart';
 import 'package:flutter/material.dart';
 
-Future showLoginModal(BuildContext context)async{
+Future showLoginModal(BuildContext context) async {
   await showModalBottomSheet(
-                  context: context,
-                  isScrollControlled: true,
-                  useSafeArea: true,
-                  // constraints: BoxConstraints(
-                  //   maxHeight: MediaQuery.of(context).size.height*0.5
-                  // ),
-                  // isDismissible: true,
-                  backgroundColor: Colors.transparent,
-                  builder: (context) => BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                      child: const LoginModal()),
-                );
+    context: context,
+    isScrollControlled: true,
+    useSafeArea: true,
+    // constraints: BoxConstraints(
+    //   maxHeight: MediaQuery.of(context).size.height*0.5
+    // ),
+    // isDismissible: true,
+    backgroundColor: Colors.transparent,
+    builder: (context) => BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+        child: Padding(
+          padding: MediaQuery.of(context).viewInsets,
+          child: const LoginModal(),
+        )),
+  );
 }
-Future showSignupModal(BuildContext context)async{
+
+Future showSignupModal(BuildContext context) async {
   await showModalBottomSheet(
-                  context: context,
-                  isScrollControlled: true,
-                  useSafeArea: true,
-                  // constraints: BoxConstraints(
-                  //   maxHeight: MediaQuery.of(context).size.height*0.5
-                  // ),
-                  // isDismissible: true,
-                  backgroundColor: Colors.transparent,
-                  builder: (context) => BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                      child: const SignupModal()),
-                );
+    context: context,
+    isScrollControlled: true,
+    useSafeArea: true,
+    // constraints: BoxConstraints(
+    //   maxHeight: MediaQuery.of(context).size.height*0.5
+    // ),
+    // isDismissible: true,
+    backgroundColor: Colors.transparent,
+    builder: (context) => BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+        child: Padding(
+          padding: MediaQuery.of(context).viewInsets,
+          child: const SignupModal(),
+        )),
+  );
 }
